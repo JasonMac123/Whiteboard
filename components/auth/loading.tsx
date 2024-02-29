d2868bae797d5b3a7249b768d696ef283e567b4a
JasonSnow123 <m.jason.2018@gmail.com>
2024-02-29 15:17:18 -0500

    Removes next js logo and updates loading clerk authetication with whiteboard logo

--- components/auth/loading.tsx
+++ components/auth/loading.tsx
@@ -4,7 +4,7 @@ export const Loading = () => {
   return (
     <div className="h-full w-full flex flex-col gap-y-4 justify-center items-center">
       <Image
-        src={"/logo.svg"}
+        src={"../../public/WhiteBoard.png"}
         alt="Logo"
         width={120}
         height={120}
