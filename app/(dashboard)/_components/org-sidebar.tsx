126e9024e5f8ceec3b552dca1ff9932b356f2831
JasonSnow123 <m.jason.2018@gmail.com>
2024-03-01 15:58:37 -0500

    Adds sidebar logo

--- app/(dashboard)/_components/org-sidebar.tsx
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -1,7 +1,32 @@
 "use client";
 
+import Link from "next/link";
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
+
 export const OrgSidebar = () => {
   return (
-    <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5"></div>
+    <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5">
+      <Link href={"/"}>
+        <div className="flex items-center gap-x-2">
+          <Image
+            src={"../../../public/WhiteBoard.png"}
+            alt="Logo"
+            height={60}
+            width={60}
+          />
+          <span className={cn("font-semibold text-2xl", font.className)}>
+            Whiteboard
+          </span>
+        </div>
+      </Link>
+    </div>
   );
 };
