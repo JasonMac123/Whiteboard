7d9642e92b030ef40f5bdb7fd3d3036978ef1300
JasonSnow123 <m.jason.2018@gmail.com>
2024-03-04 16:21:57 -0500

    Retrieves query params from url and fetches appriopriate boards

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -1,3 +1,13 @@
-export const BoardList = () => {
+"use client";
+
+interface BoardListProps {
+  orgId: string;
+  query: {
+    search?: string;
+    favourites?: string;
+  };
+}
+
+export const BoardList = ({ orgId, query }: BoardListProps) => {
   return <div></div>;
 };
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -5,12 +5,23 @@ import { useOrganization } from "@clerk/nextjs";
 import { Empty } from "./_components/empty";
 import { BoardList } from "./_components/board-list";
 
-const DashboardPage = () => {
+interface DashboardPageProps {
+  searchParams: {
+    search?: string;
+    favourites?: string;
+  };
+}
+
+const DashboardPage = ({ searchParams }: DashboardPageProps) => {
   const { organization } = useOrganization();
 
   return (
     <div className="flex-1 h-[calc(100%-80px)]">
-      {!organization ? <Empty /> : <BoardList />}
+      {!organization ? (
+        <Empty />
+      ) : (
+        <BoardList orgId={organization.id} query={searchParams} />
+      )}
     </div>
   );
 };
