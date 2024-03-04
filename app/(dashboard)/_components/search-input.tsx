8322d683e0e0565b0c43e201839a36d35688fe6f
JasonSnow123 <m.jason.2018@gmail.com>
2024-03-03 21:59:56 -0500

    Installs packages to help query for boards, and styles input and icon

--- app/(dashboard)/_components/search-input.tsx
+++ app/(dashboard)/_components/search-input.tsx
@@ -1,5 +1,19 @@
 "use client";
 
+import { ChangeEvent, useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import qs from "query-string";
+import { useDebounce } from "usehooks-ts";
+
+import { Search } from "lucide-react";
+
+import { Input } from "@/components/ui/input";
+
 export const SearchInput = () => {
-  return <div></div>;
+  return (
+    <div className="w-full relative">
+      <Search className="absolute top-1/2 left-3 transform -translate-y-1/2 text-muted-foreground h-4 w-4" />
+      <Input className="w-full max-w-[650px] pl-9" placeholder="Search here" />
+    </div>
+  );
 };
