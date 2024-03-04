25e5a12e120c8e132f4a696da79e067a4712a47d
JasonSnow123 <m.jason.2018@gmail.com>
2024-03-03 22:07:10 -0500

    Adds query search function using updated debounced value function to update query url

--- app/(dashboard)/_components/search-input.tsx
+++ app/(dashboard)/_components/search-input.tsx
@@ -3,17 +3,41 @@
 import { ChangeEvent, useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import qs from "query-string";
-import { useDebounce } from "usehooks-ts";
+import { useDebounceValue } from "usehooks-ts";
 
 import { Search } from "lucide-react";
 
 import { Input } from "@/components/ui/input";
 
 export const SearchInput = () => {
+  const router = useRouter();
+  const [debouncedValue, setDebouncedValue] = useDebounceValue("", 500);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setDebouncedValue(e.target.value);
+  };
+
+  useEffect(() => {
+    const url = qs.stringifyUrl(
+      {
+        url: "/",
+        query: {
+          search: debouncedValue,
+        },
+      },
+      { skipEmptyString: true, skipNull: true }
+    );
+  }, [debouncedValue, router]);
+
   return (
     <div className="w-full relative">
       <Search className="absolute top-1/2 left-3 transform -translate-y-1/2 text-muted-foreground h-4 w-4" />
-      <Input className="w-full max-w-[650px] pl-9" placeholder="Search here" />
+      <Input
+        className="w-full max-w-[650px] pl-9"
+        placeholder="Search here"
+        onChange={handleChange}
+        value={debouncedValue}
+      />
     </div>
   );
 };
