0c14e56e4c9a68c0a9bd13bacd0327a1e5ed3c79
JasonSnow123 <m.jason.2018@gmail.com>
2024-03-05 15:32:01 -0500

    Adds text to describe invite member button

--- app/(dashboard)/_components/navbar/invite-button.tsx
+++ app/(dashboard)/_components/navbar/invite-button.tsx
@@ -10,6 +10,7 @@ export const InviteButton = () => {
       <DialogTrigger asChild>
         <Button variant="outline">
           <Plus className="h-4 w-4 mr-2" />
+          Invite members
         </Button>
       </DialogTrigger>
       <DialogContent className="p-0 bg-transparent border-none max-w-[734px]">
