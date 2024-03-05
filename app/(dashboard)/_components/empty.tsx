29bbda383c5bc51c480e7467ad6b265f7e094237
JasonSnow123 <m.jason.2018@gmail.com>
2024-03-05 15:30:32 -0500

    Adds no organization image and adds attribution to icon

--- app/(dashboard)/_components/empty.tsx
+++ app/(dashboard)/_components/empty.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
@@ -7,10 +8,21 @@ import { CreateOrganization } from "@clerk/nextjs";
 export const Empty = () => {
   return (
     <div className="h-full flex flex-col items-center justify-center">
-      <Image src="" alt="empty" height={300} width={300} />
+      <Image
+        src="/images/noticeboard.png"
+        alt="empty"
+        height={400}
+        width={400}
+      />
+      <Link
+        href="https://www.flaticon.com/free-icons/sticky-notes"
+        className="text-xs text-muted-foreground text-neutral-700"
+      >
+        @Icon created by Freepik - Flaticon
+      </Link>
       <h2 className="text-3xl font-semibold mt-6">Welcome to Whiteboard</h2>
       <p className="text-muted-foreground text-sm mt-2">
-        Create an organization
+        Create an organization to get started
       </p>
       <div className="mt-6">
         <Dialog>
