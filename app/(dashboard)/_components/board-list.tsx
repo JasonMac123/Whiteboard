eae1f66ceee0c0266ab6db6c3d5f419d2fba4c2d
JasonSnow123 <m.jason.2018@gmail.com>
2024-03-05 18:16:17 -0500

    Adds empty message for when no boards are found for favourites and search results

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { EmptyState } from "./empty-state";
+
 interface BoardListProps {
   orgId: string;
   query: {
@@ -12,11 +14,25 @@ export const BoardList = ({ orgId, query }: BoardListProps) => {
   const data = [];
 
   if (!data.length && query.search) {
-    return <div>Search not found.</div>;
+    return (
+      <EmptyState
+        image={"/images/magnifying-glass.png"}
+        attributionLink="https://www.flaticon.com/free-icons/magnifying-glass"
+        error="No searchs found"
+        errorMessage="Try searching using another term"
+      />
+    );
   }
 
   if (!data.length && query.favourites) {
-    return <div>No favourites</div>;
+    return (
+      <EmptyState
+        image={"/images/star.png"}
+        attributionLink="https://www.flaticon.com/free-icons/star"
+        error="No favourites"
+        errorMessage="Favourite a board first!"
+      />
+    );
   }
 
   if (!data.length) {
