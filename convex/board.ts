8a53eae535bc794aecd7b1270709f137e9db87c7
JasonSnow123 <m.jason.2018@gmail.com>
2024-03-06 15:16:28 -0500

    Adds default placeholder image instead of random placeholder

--- convex/board.ts
+++ convex/board.ts
@@ -2,8 +2,6 @@ import { v } from "convex/values";
 
 import { mutation } from "./_generated/server";
 
-const images = ["/placeholder/1.svg", "/placeholder/2.svg"];
-
 export const create = mutation({
   args: {
     orgId: v.string(),
@@ -16,14 +14,12 @@ export const create = mutation({
       throw new Error("Unauthorized");
     }
 
-    const randomImage = images[Math.floor(Math.random() * images.length)];
-
     const board = await ctx.db.insert("boards", {
       title: args.title,
       orgId: args.orgId,
       authorId: identity.subject,
       authorName: identity.name!,
-      imageUrl: randomImage,
+      imageUrl: "/images/placeholder.svg",
     });
 
     return board;
