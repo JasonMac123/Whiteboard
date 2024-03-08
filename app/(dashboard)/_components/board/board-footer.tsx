cd692a298d96e02a23138667878c7277a09e6196
JasonSnow123 <m.jason.2018@gmail.com>
2024-03-08 17:26:38 -0500

    Adds styling effect for board card information for when user hovers over board

--- app/(dashboard)/_components/board/board-footer.tsx
+++ app/(dashboard)/_components/board/board-footer.tsx
@@ -21,5 +21,22 @@ export const BoardFooter = ({
   onClick,
   disabled,
 }: BoardFooterProps) => {
-  return <div></div>;
+  return (
+    <div className="relative bg-white p-3">
+      <p className="text-[13px] truncate max-w-[calc(100%-20px)]">{title}</p>
+      <p className="opacity-0 group-hover:opacity-100 transition-opacity text-[11px] text-muted-foreground truncate">
+        {authorLabel}, {dateLabel}
+      </p>
+      <button
+        disabled={disabled}
+        onClick={onClick}
+        className={cn(
+          "opacity-0 group-hover:opacity-100 transition absoolute top-3 right-3 text-muted-foreground hover:text-yellow-600",
+          favourite && "fill-yellow-600 text-yellow-600"
+        )}
+      >
+        <Star />
+      </button>
+    </div>
+  );
 };
