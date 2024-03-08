b9e760dcc0642d90a46ca9108c8e11877dcebe23
JasonSnow123 <m.jason.2018@gmail.com>
2024-03-07 19:36:52 -0500

    Styles image component and adds dark hover effect on image hover

--- app/(dashboard)/_components/board/board-card.tsx
+++ app/(dashboard)/_components/board/board-card.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import Link from "next/link";
 
 interface BoardCardProps {
@@ -21,5 +22,14 @@ export const BoardCard = ({
   createdAt,
   orgId,
 }: BoardCardProps) => {
-  return <div></div>;
+  return (
+    <Link href={`/board/${id}`}>
+      <div className="group aspect-[100/127] border rounded-lg flex flex-col justify-between overflow-hidden">
+        <div className="relative flex-1 bg-amber-50">
+          <Image src={imageUrl} alt={title} fill className="object-fit" />
+          <div className="opacity-0 group-hover:opacity-50 transition-opacity h-full w-full bg-black"></div>
+        </div>
+      </div>
+    </Link>
+  );
 };
