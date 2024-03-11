e2c0c6da9e8a2a967935e3831563f4831ee8f64c
JasonSnow123 <m.jason.2018@gmail.com>
2024-03-11 15:46:32 -0400

    Adds edit board card to dropmenu for board cards

--- components/action-menu.tsx
+++ components/action-menu.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { DropdownMenuContentProps } from "@radix-ui/react-dropdown-menu";
-import { Link2, Trash2 } from "lucide-react";
+import { Link2, Pencil, Trash2 } from "lucide-react";
 import { toast } from "sonner";
 
 import { api } from "@/convex/_generated/api";
 import { useApiMutation } from "@/hooks/use-api-mutation";
+
+import { useRenameDialog } from "@/store/rename-dialog";
 
 import {
   DropdownMenu,
@@ -31,6 +33,7 @@ export const ActionMenu = ({
   id,
   title,
 }: ActionMenuProps) => {
+  const { onOpen } = useRenameDialog();
   const { mutate, pending } = useApiMutation(api.board.remove);
 
   const onDelete = () => {
@@ -58,6 +61,13 @@ export const ActionMenu = ({
         <DropdownMenuItem className="p-3 cursor-pointer" onClick={onCopyLink}>
           <Link2 className="h-4 w-4 mr-2" />
           Copy Board Link
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          className="p-3 cursor-pointer"
+          onClick={() => onOpen(id, title)}
+        >
+          <Pencil className="h-4 w-4 mr-2" />
+          Rename
         </DropdownMenuItem>
         <ConfirmDialog
           header="Delete board?"
