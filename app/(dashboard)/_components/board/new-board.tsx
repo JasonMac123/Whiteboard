6b7b8e0fb79589813c1d1495ee9bda5cd80ab29d
JasonSnow123 <m.jason.2018@gmail.com>
2024-03-10 21:51:59 -0400

    Adds board creation functionality to create board

--- app/(dashboard)/_components/board/new-board.tsx
+++ app/(dashboard)/_components/board/new-board.tsx
@@ -1,7 +1,13 @@
 "use client";
 
-import { cn } from "@/lib/utils";
 import { Plus } from "lucide-react";
+
+import { toast } from "sonner";
+
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { api } from "@/convex/_generated/api";
+
+import { cn } from "@/lib/utils";
 
 interface NewBaordProps {
   orgId: string;
@@ -9,13 +15,30 @@ interface NewBaordProps {
 }
 
 export const NewBoard = ({ orgId, disabled }: NewBaordProps) => {
+  const { mutate, pending } = useApiMutation(api.board.create);
+
+  const onClick = () => {
+    if (!orgId) return;
+
+    mutate({
+      orgId: orgId,
+      title: "Untitled",
+    })
+      .then((id) => {
+        toast.success("Board created");
+      })
+      .catch(() => {
+        toast.error("Failed to create board");
+      });
+  };
+
   return (
     <button
       disabled={disabled}
-      onClick={() => {}}
+      onClick={onClick}
       className={cn(
         "col-span-1 aspect-[100/127] bg-sky-400 rounded-lg hover:bg-sky-700 flex flex-col items-center justify-center py-6",
-        disabled && "opacity-75"
+        (pending || disabled) && "opacity-75"
       )}
     >
       <div />
