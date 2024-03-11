d1808c802216c793370e551c5981c5226cab7401
JasonSnow123 <m.jason.2018@gmail.com>
2024-03-11 14:53:58 -0400

    Adds delete option for board inside drop down action menu

--- components/action-menu.tsx
+++ components/action-menu.tsx
@@ -1,8 +1,11 @@
 "use client";
 
 import { DropdownMenuContentProps } from "@radix-ui/react-dropdown-menu";
-import { Link2 } from "lucide-react";
+import { Link2, Trash2 } from "lucide-react";
 import { toast } from "sonner";
+
+import { api } from "@/convex/_generated/api";
+import { useApiMutation } from "@/hooks/use-api-mutation";
 
 import {
   DropdownMenu,
@@ -26,6 +29,14 @@ export const ActionMenu = ({
   id,
   title,
 }: ActionMenuProps) => {
+  const { mutate, pending } = useApiMutation(api.board.remove);
+
+  const onDelete = () => {
+    mutate({ id })
+      .then(() => toast.success("Board deleted"))
+      .catch(() => toast.error("Failed to delete board"));
+  };
+
   const onCopyLink = () => {
     navigator.clipboard
       .writeText(`${window.location.origin}/board/${id}`)
@@ -45,6 +56,10 @@ export const ActionMenu = ({
         <DropdownMenuItem className="p-3 cursor-pointer" onClick={onCopyLink}>
           <Link2 className="h-4 w-4 mr-2" />
           Copy Board Link
+        </DropdownMenuItem>
+        <DropdownMenuItem className="p-3 cursor-pointer" onClick={onDelete}>
+          <Trash2 className="h-4 w-4 mr-2" />
+          Delete Board
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
