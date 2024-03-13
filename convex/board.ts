0b469e207db3fd6034de1d62186d0671eb473e52
JasonSnow123 <m.jason.2018@gmail.com>
2024-03-13 15:13:16 -0400

    Adds back board methods that were removed

--- convex/board.ts
+++ convex/board.ts
@@ -39,6 +39,73 @@ export const remove = mutation({
   },
 });
 
+export const update = mutation({
+  args: { id: v.id("boards"), title: v.string() },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Unauthorized");
+    }
+
+    const title = args.title.trim();
+
+    if (!title) {
+      throw new Error("Title is required");
+    }
+
+    if (title.length < 8 || title.length > 50) {
+      throw new Error(
+        "Title minimum length is 8 characters and max length is 50 characters"
+      );
+    }
+
+    const board = await ctx.db.patch(args.id, {
+      title: args.title,
+    });
+
+    return board;
+  },
+});
+
+export const favourite = mutation({
+  args: { id: v.id("boards"), orgId: v.string() },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Unauthorized");
+    }
+
+    const board = await ctx.db.get(args.id);
+
+    if (!board) {
+      throw new Error("Board not found");
+    }
+
+    const userId = identity.subject;
+
+    const userFavourites = await ctx.db
+      .query("userFavourites")
+      .withIndex("by_user_board_org", (q) =>
+        q.eq("boardId", board._id).eq("orgId", board.orgId).eq("userId", userId)
+      )
+      .unique();
+
+    if (userFavourites) {
+      throw new Error("Board already favourited");
+    }
+
+    await ctx.db.insert("userFavourites", {
+      userId,
+      boardId: board._id,
+      orgId: args.orgId,
+    });
+
+    return board;
+  },
+});
+
 export const unfavourite = mutation({
   args: { id: v.id("boards"), orgId: v.string() },
   handler: async (ctx, args) => {
