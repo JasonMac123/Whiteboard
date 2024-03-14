a1686bfbab13dcc2aac7fa3b73ad81c0716c0891
JasonSnow123 <m.jason.2018@gmail.com>
2024-03-14 15:14:20 -0400

    Creates backdrop for users to create storyboards

---
+++ app/board/[boardId]/_components/whiteboard.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+export const WhiteBoard = () => {
+  return (
+    <main className="h-full w-full relative bg-neutral-100 touch-none"></main>
+  );
+};
---
+++ app/board/[boardId]/page.tsx
@@ -0,0 +1,7 @@
+import { WhiteBoard } from "./_components/whiteboard";
+
+const BoardIdPage = () => {
+  return <WhiteBoard />;
+};
+
+export default BoardIdPage;
