09173a9ff63d0578a267532b329fe29dd2442347
JasonSnow123 <m.jason.2018@gmail.com>
2024-03-14 15:29:58 -0400

    Creates whiteboard layout components for board info, drawing kit, and users involved

--- app/board/[boardId]/_components/whiteboard.tsx
+++ app/board/[boardId]/_components/whiteboard.tsx
@@ -1,7 +1,15 @@
 "use client";
 
+import { BoardInfo } from "./board-info";
+import { BoardMembers } from "./board-members";
+import { ToolKit } from "./tool-kit";
+
 export const WhiteBoard = () => {
   return (
-    <main className="h-full w-full relative bg-neutral-100 touch-none"></main>
+    <main className="h-full w-full relative bg-neutral-100 touch-none">
+      <BoardInfo />
+      <BoardMembers />
+      <ToolKit />
+    </main>
   );
 };
