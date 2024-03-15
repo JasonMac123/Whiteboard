b5048dc450d2b1ba73947a67b034620c24c10a6b
JasonSnow123 <m.jason.2018@gmail.com>
2024-03-15 15:34:49 -0400

    Adds website logo to board and adds redirection back to homepage

--- app/board/[boardId]/_components/board-info.tsx
+++ app/board/[boardId]/_components/board-info.tsx
@@ -1,8 +1,41 @@
 "use client";
 
-export const BoardInfo = () => {
+import Link from "next/link";
+import Image from "next/image";
+import { useQuery } from "convex/react";
+
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+
+import { Button } from "@/components/ui/button";
+
+interface BoardInfoProps {
+  boardId: string;
+}
+
+export const BoardInfo = ({ boardId }: BoardInfoProps) => {
+  const data = useQuery(api.board.get, { id: boardId as Id<"boards"> });
+
+  if (!data) {
+    return <BoardInfoSkeleton />;
+  }
+
   return (
-    <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md"></div>
+    <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md">
+      <Button asChild className="px-2" variant={"board"}>
+        <Link href={"/"}>
+          <Image
+            src={"/images/WhiteBoard.png"}
+            alt="Logo"
+            height={50}
+            width={50}
+          />
+          <span className="font-semibold text-xl ml-2 text-black">
+            WhiteBoard
+          </span>
+        </Link>
+      </Button>
+    </div>
   );
 };
 
