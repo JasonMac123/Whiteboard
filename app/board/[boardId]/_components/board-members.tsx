4fef073ea45a5fd8a45988c05f606d82c7840332
JasonSnow123 <m.jason.2018@gmail.com>
2024-03-17 21:54:02 -0400

    Displays current user logged in and displays how much other members and styles components

--- app/board/[boardId]/_components/board-members.tsx
+++ app/board/[boardId]/_components/board-members.tsx
@@ -12,16 +12,30 @@ export const BoardMembers = () => {
   return (
     <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md">
       <div className="flex gap-x-2">
-        {users.slice(0, 4).map(({ connectionId, info }) => {
+        {users.slice(0, 3).map(({ connectionId, info }) => {
           return (
             <UserAvatar
               key={connectionId}
               src={info?.picture}
               name={info?.name}
-              fallback={info?.name || "T"}
+              fallback={info?.name || "Team Member"}
             />
           );
         })}
+        {currentUser && (
+          <UserAvatar
+            src={currentUser.info?.picture}
+            name={currentUser.info?.name}
+            fallback={`${currentUser.info?.name} You`}
+          />
+        )}
+
+        {users.length > 3 && (
+          <UserAvatar
+            name={`${users.length - 3} more`}
+            fallback={`+${users.length - 3}`}
+          />
+        )}
       </div>
     </div>
   );
