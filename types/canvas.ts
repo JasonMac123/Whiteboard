293c45f4a6e14f9274bc05be167ff2536086937a
JasonSnow123 <m.jason.2018@gmail.com>
2024-03-18 15:26:20 -0400

    Introduces different types of drawing tools for canvas

--- types/canvas.ts
+++ types/canvas.ts
@@ -1,21 +1,52 @@
+import { LayerType } from "./layer";
+
+export type Point = {
+  x: number;
+};
+
+export type XYWH = {
+  x: number;
+  y: number;
+  width: number;
+  heigth: number;
+};
+
+export enum Side {
+  Top = 1,
+  Bottom = 2,
+  Left = 4,
+  Right = 8,
+}
+
 export type CanvasState =
   | {
       mode: CanvasMode.None;
     }
   | {
       mode: CanvasMode.Pressing;
+      origin: Point;
     }
   | {
       mode: CanvasMode.SelectionNet;
+      origin: Point;
+      current?: Point;
     }
   | {
       mode: CanvasMode.Translating;
+      current: Point;
     }
   | {
       mode: CanvasMode.Inserting;
+      layerType:
+        | LayerType.Ellipse
+        | LayerType.Rectangle
+        | LayerType.Text
+        | LayerType.Note;
     }
   | {
       mode: CanvasMode.Resizing;
+      intialBounds: XYWH;
+      corner: Side;
     }
   | {
       mode: CanvasMode.Pencil;
