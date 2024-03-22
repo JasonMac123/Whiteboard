732d6787074c50bdb660454234a4444780168a6c
JasonSnow123 <m.jason.2018@gmail.com>
2024-03-21 22:58:06 -0400

    Displays highlighted area based on selection area

--- app/board/[boardId]/_components/selection-box.tsx
+++ app/board/[boardId]/_components/selection-box.tsx
@@ -1,14 +1,44 @@
 "use client";
 
 import { memo } from "react";
+import { useSelf, useStorage } from "@/liveblocks.config";
+
+import { Side, XYWH } from "@/types/whiteboard";
+import { LayerType } from "@/types/layer";
+import { useSelectionArea } from "@/hooks/use-selection-area";
 
 interface SelectionBoxProps {
-  onResizeHandlePointerDown: () => void;
+  onResizeHandlePointerDown: (corner: Side, intialBounds: XYWH) => void;
 }
 
 export const SelectionBox = memo(
   ({ onResizeHandlePointerDown }: SelectionBoxProps) => {
-    return <div></div>;
+    const layerId = useSelf((me) =>
+      me.presence.selection.length === 1 ? me.presence.selection[0] : null
+    );
+
+    const showingHandles = useStorage(
+      (root) => layerId && root.layers.get(layerId)?.type !== LayerType.Path
+    );
+
+    const bounds = useSelectionArea();
+
+    if (!bounds) {
+      return null;
+    }
+
+    return (
+      <>
+        <rect
+          className="fill-transparent stroke-blue-500 stroke-1 pointer-events-none"
+          style={{ transform: `translate(${bounds.x}px, ${bounds.y}px)` }}
+          x={0}
+          y={0}
+          width={bounds.width}
+          height={bounds.heigth}
+        />
+      </>
+    );
   }
 );
 
