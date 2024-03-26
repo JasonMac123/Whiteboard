ad2f607a43269d7ffdc7c21f1535040c8c770465
JasonSnow123 <m.jason.2018@gmail.com>
2024-03-26 17:23:58 -0400

    Styles circle ellipse layer with selected colouring and adds on click function

--- app/board/[boardId]/_components/layers/ellipse-layer.tsx
+++ app/board/[boardId]/_components/layers/ellipse-layer.tsx
@@ -12,5 +12,18 @@ interface EllipseLayerProps {
 }
 
 export const Ellipse = ({ id, layer, onPointerDown, selectionColour }: EllipseLayerProps) => {
-  return <ellipse />;
+  return (
+    <ellipse
+      className="drop-shadow-md"
+      onPointerDown={(e) => onPointerDown(e, id)}
+      style={{ transform: `translate(${layer.x}px, ${layer.y}px)` }}
+      cx={layer.width / 2}
+      cy={layer.height / 2}
+      rx={layer.width / 2}
+      ry={layer.height / 2}
+      fill={layer.fill ? rgbHex(layer.fill.r, layer.fill.g, layer.fill.b) : "#000"}
+      stroke={selectionColour || "transparent"}
+      strokeWidth={1}
+    />
+  );
 };
