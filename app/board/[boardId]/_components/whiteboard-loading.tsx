01acb76d28e9778174a6006b136d997c99703ddb
JasonSnow123 <m.jason.2018@gmail.com>
2024-03-28 19:44:32 -0400

    Uses whiteboard loading screen when loading up liveblock rooms and fixes import issues with whiteboard skeleton component

--- app/board/[boardId]/_components/whiteboard-loading.tsx
+++ app/board/[boardId]/_components/whiteboard-loading.tsx
@@ -1,7 +1,7 @@
 import { Loader2 } from "lucide-react";
 
-import { BoardInfoSkeleton } from "./board-info";
-import { BoardMembersSkeleton } from "./board-members";
+import { BoardInfoSkeleton } from "./board-layout/board-info";
+import { BoardMembersSkeleton } from "./board-layout/board-members";
 import { ToolKitSkeleton } from "./board-layout/tool-kit/index";
 
 export const WhiteBoardLoading = () => {
