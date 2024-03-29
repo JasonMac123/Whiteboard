db90746baa81e898a8dbb3ccbd3be16c40f6c9a1
JasonSnow123 <m.jason.2018@gmail.com>
2024-03-28 20:03:48 -0400

    Fixes typo for resizing height would resize width

--- lib/resizeBounds.ts
+++ lib/resizeBounds.ts
@@ -1,7 +1,12 @@
 import { Point, Side, XYWH } from "@/types/whiteboard";
 
 export function resizeBounds(bounds: XYWH, corner: Side, point: Point): XYWH {
-  const result = { x: bounds.x, y: bounds.y, width: bounds.width, height: bounds.height };
+  const result = {
+    x: bounds.x,
+    y: bounds.y,
+    width: bounds.width,
+    height: bounds.height,
+  };
 
   if ((corner & Side.Left) === Side.Left) {
     result.x = Math.min(point.x, bounds.x + bounds.width);
@@ -15,12 +20,12 @@ export function resizeBounds(bounds: XYWH, corner: Side, point: Point): XYWH {
 
   if ((corner & Side.Top) === Side.Top) {
     result.y = Math.min(point.y, bounds.y + bounds.height);
-    result.width = Math.abs(bounds.y + bounds.height - point.y);
+    result.height = Math.abs(bounds.y + bounds.height - point.y);
   }
 
   if ((corner & Side.Bottom) === Side.Bottom) {
     result.y = Math.min(point.y, bounds.y);
-    result.width = Math.abs(point.y - bounds.y);
+    result.height = Math.abs(point.y - bounds.y);
   }
 
   return result;
