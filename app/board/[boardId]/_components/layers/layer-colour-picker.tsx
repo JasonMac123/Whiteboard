fa03b999a36f6c8bcb4e09f0ebda01ef83f4fa37
JasonSnow123 <m.jason.2018@gmail.com>
2024-03-29 17:58:16 -0400

    Changes colour options to be more diverse

--- app/board/[boardId]/_components/layers/layer-colour-picker.tsx
+++ app/board/[boardId]/_components/layers/layer-colour-picker.tsx
@@ -10,14 +10,14 @@ interface LayerColourPickerProps {
 export const LayerColourPicker = ({ onChange }: LayerColourPickerProps) => {
   return (
     <div className="flex flex-wrap gap-2 items-center max-w-[164px] pr-2 mr-2 border-r border-neutral-200">
+      <ColourButton onClick={onChange} colour={{ r: 243, g: 72, b: 35 }} />
+      <ColourButton onClick={onChange} colour={{ r: 255, g: 250, b: 170 }} />
+      <ColourButton onClick={onChange} colour={{ r: 70, g: 202, b: 100 }} />
+      <ColourButton onClick={onChange} colour={{ r: 41, g: 149, b: 239 }} />
+      <ColourButton onClick={onChange} colour={{ r: 162, g: 109, b: 244 }} />
+      <ColourButton onClick={onChange} colour={{ r: 250, g: 143, b: 41 }} />
       <ColourButton onClick={onChange} colour={{ r: 0, g: 0, b: 0 }} />
       <ColourButton onClick={onChange} colour={{ r: 255, g: 255, b: 255 }} />
-      <ColourButton onClick={onChange} colour={{ r: 243, g: 79, b: 40 }} />
-      <ColourButton onClick={onChange} colour={{ r: 255, g: 250, b: 177 }} />
-      <ColourButton onClick={onChange} colour={{ r: 68, g: 200, b: 100 }} />
-      <ColourButton onClick={onChange} colour={{ r: 243, g: 79, b: 40 }} />
-      <ColourButton onClick={onChange} colour={{ r: 255, g: 250, b: 177 }} />
-      <ColourButton onClick={onChange} colour={{ r: 68, g: 200, b: 100 }} />
     </div>
   );
 };
@@ -35,8 +35,8 @@ const ColourButton = ({ onClick, colour }: ColourButtonProps) => {
     >
       <div
         className="h-8 w-8 rounded-md border border-neutral-300"
-        style={{ background: rgbHex(colour.r, colour.g, colour.b) }}
-      ></div>
+        style={{ background: `#${rgbHex(colour.r, colour.g, colour.b)}` }}
+      />
     </button>
   );
 };
