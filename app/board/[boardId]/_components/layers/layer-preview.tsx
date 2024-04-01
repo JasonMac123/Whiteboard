624e30a467035b12ae154b276adf587d173a526f
JasonSnow123 <m.jason.2018@gmail.com>
2024-04-01 16:26:23 -0400

    Creates component to display free hand drawing using perfect freehand package

--- app/board/[boardId]/_components/layers/layer-preview.tsx
+++ app/board/[boardId]/_components/layers/layer-preview.tsx
@@ -8,6 +8,8 @@ import { Rectangle } from "./rectangle-layer";
 import { Ellipse } from "./ellipse-layer";
 import { Text } from "./text-layer";
 import { Note } from "./note-layer";
+import { Path } from "./path-layer";
+import rgbHex from "rgb-hex";
 
 interface LayerPreviewProps {
   id: string;
@@ -61,7 +63,17 @@ export const LayerPreview = memo(
           />
         );
       case LayerType.Path:
-        return <div></div>;
+        return (
+          <Path
+            key={id}
+            x={layer.x}
+            y={layer.y}
+            fill={layer.fill ? `#${rgbHex(layer.fill.r, layer.fill.g, layer.fill.b)}` : "#000"}
+            points={layer.points}
+            onPointerDown={(e) => onLayerPointerDown(e, id)}
+            stroke={selectionColour}
+          />
+        );
       default:
         console.log("error unkonwn type");
         return null;
