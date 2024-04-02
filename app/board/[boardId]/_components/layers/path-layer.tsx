107ae4f68c48ce69b71461d76c11ff67c125e413
JasonSnow123 <m.jason.2018@gmail.com>
2024-04-02 15:30:21 -0400

    Renders pen drawing using coordinates after user finishing drawing

--- app/board/[boardId]/_components/layers/path-layer.tsx
+++ app/board/[boardId]/_components/layers/path-layer.tsx
@@ -1,3 +1,4 @@
+import { getSVG } from "@/lib/getSVG";
 import getStroke from "perfect-freehand";
 
 interface PathProps {
@@ -10,5 +11,19 @@ interface PathProps {
 }
 
 export const Path = ({ x, y, points, fill, onPointerDown, stroke }: PathProps) => {
-  return <path />;
+  return (
+    <path
+      className="drop-shadow-md"
+      onPointerDown={onPointerDown}
+      d={getSVG(getStroke(points, { size: 16, thinning: 0.5, smoothing: 0.5, streamline: 0.5 }))}
+      style={{
+        transform: `translate(${x}px, ${y}px)`,
+      }}
+      x={0}
+      y={0}
+      fill={fill}
+      stroke={stroke}
+      strokeWidth={1}
+    />
+  );
 };
