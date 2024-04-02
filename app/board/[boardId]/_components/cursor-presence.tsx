6b31ad89fe15d64e1533c904490b5ac3c70f8597
JasonSnow123 <m.jason.2018@gmail.com>
2024-04-02 16:27:54 -0400

    Renders all other users drawing real-time for the user on screen

--- app/board/[boardId]/_components/cursor-presence.tsx
+++ app/board/[boardId]/_components/cursor-presence.tsx
@@ -2,9 +2,12 @@
 
 import { memo } from "react";
 
-import { useOthersConnectionIds } from "@/liveblocks.config";
+import { useOthersConnectionIds, useOthersMapped } from "@/liveblocks.config";
 
 import { Cursor } from "./cursor";
+import { shallow } from "@liveblocks/client";
+import { Path } from "./layers/path-layer";
+import rgbHex from "rgb-hex";
 
 const Cursors = () => {
   const ids = useOthersConnectionIds();
@@ -18,9 +21,43 @@ const Cursors = () => {
   );
 };
 
+const PencilDrafts = () => {
+  const others = useOthersMapped(
+    (other) => ({
+      PencilDraft: other.presence.pencilDraft,
+      penColour: other.presence.penColour,
+    }),
+    shallow
+  );
+
+  return (
+    <>
+      {others.map(([key, other]) => {
+        if (other.PencilDraft) {
+          return (
+            <Path
+              key={key}
+              x={0}
+              y={0}
+              points={other.PencilDraft}
+              fill={
+                other.penColour
+                  ? `#${rgbHex(other.penColour.r, other.penColour.g, other.penColour.b)}`
+                  : "#000"
+              }
+            />
+          );
+        }
+        return null;
+      })}
+    </>
+  );
+};
+
 export const CursorPresence = memo(() => {
   return (
     <>
+      <PencilDrafts />
       <Cursors />
     </>
   );
